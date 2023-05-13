11afc7f1e38b3f11a165baf0574b1992226cba3f
Fabian Siffert <siffert@sipgate.de>
2023-05-13 23:02:28 +0200

    🚧 start implementing an edge to edge layout

--- app/src/main/java/de/siffertio/app/quest/MainActivity.kt
+++ app/src/main/java/de/siffertio/app/quest/MainActivity.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.animations.rememberAnimatedNavHostEngine
@@ -22,10 +21,15 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
-@OptIn(ExperimentalMaterialNavigationApi::class, ExperimentalAnimationApi::class)
+@OptIn(
+    ExperimentalMaterialNavigationApi::class,
+    ExperimentalAnimationApi::class,
+    ExperimentalMaterial3Api::class
+)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         startKoin {
             androidLogger()
             androidContext(this@MainActivity)
@@ -35,14 +39,11 @@ class MainActivity : ComponentActivity() {
             val engine = rememberAnimatedNavHostEngine()
             val navController = engine.rememberNavController()
             QuestTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
+                Scaffold() { innerPadding ->
                     DestinationsNavHost(
                         engine = engine,
                         navController = navController,
-                        modifier = Modifier.padding(2.dp),
+                        modifier = Modifier.padding(innerPadding),
                         navGraph = NavGraphs.root
                     )
                 }
--- app/src/main/java/de/siffertio/app/quest/home/Home.kt
+++ app/src/main/java/de/siffertio/app/quest/home/Home.kt
@@ -1,13 +1,27 @@
 package de.siffertio.app.quest.home
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
+import de.siffertio.app.quest.ui.theme.QuestTheme
 
 @Composable
 @Destination
 @RootNavGraph(start = true)
 fun Home() {
-    Text(text = "Home Screen")
+    QuestTheme() {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Home Screen")
+        }
+    }
 }
